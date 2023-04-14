90e4fe366576d5c35b3cc3595ecfbe7f2a4c0170
NabihSamy <108161871+NabihSamy@users.noreply.github.com>
2023-04-14 19:04:51 +0100

    "hak chako"

--- Projet_Basket_Ecomm/Projet_Basket_Ecomm/urls.py
+++ Projet_Basket_Ecomm/Projet_Basket_Ecomm/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('autentification/', include('autentification.urls')),
+    path('authentification/', include('authentification.urls')),
 ]
